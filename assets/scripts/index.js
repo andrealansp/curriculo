1d208019c27f1303392765b01f9f65f07c8465de
Andre <andrealansp@hotmail.com>
2021-11-09 23:52:43 -0300

    Criando a seção de github

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -34,21 +34,50 @@ function loadEvents() {
 }
 
 function loadGithub() {
+    bioElem = document.querySelector("#bio");
+    imgElem = document.querySelector("#profile");
+
+    repositoriesElem = document.querySelector("#repositories");
+
     const access_token = "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";
+
+    let one = "https://api.github.com/user";
+    let two = "https://api.github.com/users/andrealansp/repos";
+
+    const requestOne = axios.get(one, {
+        headers: {
+            Authorization: `token ${access_token}`,
+        },
+    });
+    const requestTwo = axios.get(two, {
+        headers: {
+            Authorization: `token ${access_token}`,
+        },
+    });
+
     axios
-        .get("https://api.github.com/user", {
-            headers: {
-                Authorization: `token ${access_token}`,
-            },
-        })
-        .then(function(response) {
-            for (x in response.data) {}
-        })
-        .catch(function(error) {
-            // handle error
-            console.log(error);
-        })
-        .then(function() {
-            // always executed
+        .all([requestOne, requestTwo])
+        .then(
+            axios.spread((...responses) => {
+                const responseOne = responses[0];
+                const responseTwo = responses[1];
+                bioElem.innerHTML = responseOne.data.bio;
+                imgElem.src = responseOne.data.avatar_url;
+
+                console.log(responseTwo);
+
+                let count = 0;
+                for (x in responseTwo.data) {
+                    repo = document.createElement("div");
+                    repo.classList.add("repos");
+                    repo.innerHTML = `<p>${responseTwo.data[count]["name"]}</p> <p>${responseTwo.data[count]["description"]}</p>`;
+                    repositoriesElem.appendChild(repo);
+                    count++;
+                }
+            })
+        )
+        .catch((errors) => {
+            // react on errors.
+            console.error(errors);
         });
 }
